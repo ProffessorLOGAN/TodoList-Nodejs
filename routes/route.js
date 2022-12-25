29e5f05cac643f7478b2175478db22bd9e1c3935
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-12-25 20:26:02 +0530

    vercel added

--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,7 @@ const homeController = require('../controllers/home_controller');
 
 console.log("router is loaded");
 
+router.get('/todolist',homeController.home);
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 
