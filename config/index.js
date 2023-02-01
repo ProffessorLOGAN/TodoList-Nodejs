db4c4a913871c050fc2a3e31cf1ab914f91c2ab6
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-02-01 20:50:52 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const {
-    DB_URL=`mongodb+srv://proffessor:logan@cluster0.adfkkil.mongodb.net/todolist`,
+    DB_URL=`mongodb+srv://proffessor:logan@cluster0.adfkkil.mongodb.net/todo`,
     PORT=3001
 
 } = process.env;
